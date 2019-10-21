0d1f1b3f4b92aa705431b6cc0f31dd38652ea0d2
Jake <jlconnerly@gmail.com>
2019-10-21 16:15:07 -0500

    Updated main storyBoard to be BookATrip

---
+++ Kids Fly/Kids Fly/View Controllers/BookATripMainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BookATripMainViewController.swift
+//  Kids Fly
+//
+//  Created by Jake Connerly on 10/21/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class BookATripMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
