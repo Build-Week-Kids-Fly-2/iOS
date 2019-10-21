527d44ea67323c702b8f337f3ba3b715a8c3b101
Jake <jlconnerly@gmail.com>
2019-10-21 16:00:47 -0500

    Added Assets

---
+++ Kids Fly/Kids Fly/CustomColors.swift
@@ -0,0 +1,16 @@
+//
+//  CustomColors.swift
+//  Kids Fly
+//
+//  Created by Jake Connerly on 10/21/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let seaGreen = UIColor(red:0.88, green:0.92, blue:0.92, alpha:1.0)
+    
+    static let buttonOrange = UIColor(red:0.96, green:0.79, blue:0.54, alpha:1.0)
+}
